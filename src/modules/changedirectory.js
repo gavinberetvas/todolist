e9e06cbeb97356a56c9f8a0032ab2e27d288d951
Gavin <gavinberetvas@ucsb.edu>
2023-05-17 10:22:14 -0700

    style

--- src/modules/changedirectory.js
+++ src/modules/changedirectory.js
@@ -5,12 +5,14 @@ function switchdirectory() {
 document.getElementById("today").addEventListener("click", function() {
     index = 'today';
     hideItems();
+    makeCurrentDirectory();
     console.log(index);
   });
 
   document.getElementById("tomorrow").addEventListener("click", function() {
     index = 'tomorrow';
     hideItems();
+    makeCurrentDirectory();
     console.log(index);
   });
 }
@@ -26,6 +28,19 @@ function hideItems() {
       element.style.display = "none";
     }
   }
+}
+
+function makeCurrentDirectory() {
+  event.target.classList.add("current");
+  const buttons = document.querySelectorAll(".directory")
+  buttons.forEach(button => {
+    if (button != event.target) {
+      button.classList.remove("current");
+    }
+  })
+  // on click 
+  // deletes current from other buttons 
+  // and adds current to this button
 }
 
 export default switchdirectory;
