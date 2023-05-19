76147edd514a17a8464d509dad7cba9c551de03b
Gavin <gavinberetvas@ucsb.edu>
2023-05-19 10:58:09 -0700

    clean code

--- src/index.js
+++ src/index.js
@@ -7,42 +7,4 @@ import { pushAllItemstoDom } from "./modules/domManipulation";
 
 modal();
 switchdirectory();
-myLibrary.getAllObjects();
 pushAllItemstoDom();
-
-
-
-
-// const dumbutton = document.getElementById("hideitems");
-// dumbutton.addEventListener("click", hideItems);
-
-// function hideItems() {
-//   let elements = document.getElementsByClassName("shownote");
-//   for (let i = 0; i < elements.length; i++) {
-//     elements[i].classList.remove("shownote");
-//   }
-// // }
-
-// function hideItems() {
-//   const elements = document.getElementsByClassName('card');
-//   for (let i = 0; i < elements.length; i++) {
-//     const element = elements[i];
-
-//     // if element has the right index value, set display to ''
-//     // otherwise hide it
-
-  
-
-//     if (element.contains(index)) {
-//       element.style.display = ""; // Show the element
-//     } else {
-//       element.style.display = "none"; // Hide the element
-//     }
-
-//     // if (element.style.display === "none") {
-//     //   element.style.display = ""; // Show the element
-//     // } else {
-//     //   element.style.display = "none"; // Hide the element
-//     // }
-//   }
-// }
