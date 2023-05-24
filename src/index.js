51a48ad9bbe4a4dcabc6a78c156818daa8e942cf
Gavin <gavinberetvas@ucsb.edu>
2023-05-24 10:28:46 -0700

    library local storage work. pushAllItemsToDom rework begin

--- src/index.js
+++ src/index.js
@@ -5,101 +5,36 @@ import switchdirectory from "./modules/switchdirectory";
 import { myLibrary } from "./modules/myLibraryObject";
 import { pushAllItemstoDom } from "./modules/domManipulation";
 import { newProjectButton } from "./modules/newProjectButton";
+import { loadFromLocalStorage } from "./modules/myLibraryObject";
 
-
-
-
+// test();
 modal();
 switchdirectory();
 newProjectButton();
+loadFromLocalStorage();
+pushAllItemstoDom();
 
-// loadFromLocalStorage();
+window.addEventListener("beforeunload", function () {
+  myLibrary.saveToLocalStorage();
+});
 
 // window.addEventListener("beforeunload", function () {
-//   myLibrary.saveToLocalStorage();
+//   let domContent = document.getElementById("main-content").innerHTML;
+//   localStorage.setItem("domContent", domContent);
 // });
 
-// function loadFromLocalStorage() {
-//   const data = localStorage.getItem("myLibrary");
-//   console.log(data);
-//   if (data) {
-//     const savedLibrary = JSON.parse(data);
-//     console.log(savedLibrary);
-//     //     Object.assign(myLibrary, savedLibrary);
-//     //   }
-//     // //initialization of object methods:
-//     myLibrary.newNote = function (event, index) {
-//       event.preventDefault();
-//       const { title, date, description } = event.target.elements;
-//       const uuid = uuidv4();
+// function test() {
+//   let domContent = localStorage.getItem("domContent");
+//   document.getElementById("main-content").innerHTML = domContent;
 
-//       this[index].push({
-//         title: title.value,
-//         date: date.value,
-//         description: description.value,
-//         id: uuid,
-//       });
-//       console.log(this);
-//       modal.classList.remove("active");
-//       overlay.classList.remove("active");
-//     };
-
-//     myLibrary.editNote = function (note, notetitle, notedate, notedescription) {
-//       let itemID = note.getAttribute("data-id");
-
-//       let editedObject = {
-//         title: `${notetitle}`,
-//         date: `${notedate}`,
-//         description: `${notedescription}`,
-//       };
-
-//       for (let key in myLibrary) {
-//         if (Array.isArray(myLibrary[key])) {
-//           const index = myLibrary[key].findIndex((item) => (item.id = itemID));
-//           if (index !== -1) {
-//             myLibrary[key][index] = editedObject;
-//             break;
-//           }
-//         }
-//       }
-//     };
-
-//     myLibrary.deleteNote = function (removeBtn) {
-//       let idValue = removeBtn.closest("[data-id]").getAttribute("data-id");
-//       for (let key in myLibrary) {
-//         if (Array.isArray(myLibrary[key])) {
-//           const index = myLibrary[key].findIndex((item) => item.id == idValue);
-//           if (index !== -1) {
-//             myLibrary[key].splice(index, 1);
-//             return;
-//           }
-//         }
-//       }
-//     };
-
-//     myLibrary.newProject = function (uuid) {
-//       myLibrary[uuid] = [];
-//     };
-
-//     myLibrary.getAllObjects = function () {
-//       const objects = [];
-
-//       for (let key in this) {
-//         if (this.hasOwnProperty(key) && Array.isArray(this[key])) {
-//           objects.push(...this[key]);
-//         }
-//       }
-
-//       console.log(objects);
-//       return objects;
-//     };
-
-//     myLibrary.saveToLocalStorage = function () {
-//       localStorage.setItem("myLibrary", JSON.stringify(this));
-//     };
-
-//     Object.assign(myLibrary, savedLibrary);
-//   }
+//   if (domContent == "" || domContent == undefined || domContent == null) {
+//     domContent = document.getElementById("main-content").innerHTML =
+//     '<div data-id="93bb33ce-7a58-4523-b660-cc0de999f8f7" class="card all 93bb33ce-7a58-4523-b660-cc0de999f8f7" style="z-index: 1; position: relative;"><div class="title" contenteditable="true">Test Note</div><input class="date" type="date"><div class="description" style="display: none;" contenteditable="true">This is a developer\'s note. </div><div class="buttonbox"><button class="button rmv" style="">X</button></div></div>';
 // }
 
-pushAllItemstoDom();
+//   // let domContent = document.getElementById("main-content").innerHTML;
+//   console.log(`DOMCONTENT VALUE: ${domContent}`);
+// }
+
+// //    const data = localStorage.getItem("myLibrary");
+// //   console.log(data);
--- src/modules/myLibraryObject.js
+++ src/modules/myLibraryObject.js
@@ -120,3 +120,86 @@ export let myLibrary = {
     localStorage.setItem('myLibrary', JSON.stringify(this));
   }
 };
+
+export function loadFromLocalStorage() {
+  const data = localStorage.getItem("myLibrary");
+  console.log(data);
+  if (data) {
+    const savedLibrary = JSON.parse(data);
+    console.log(savedLibrary);
+    //     Object.assign(myLibrary, savedLibrary);
+    //   }
+    // //initialization of object methods:
+    myLibrary.newNote = function (event, index) {
+      event.preventDefault();
+      const { title, date, description } = event.target.elements;
+      const uuid = uuidv4();
+
+      this[index].push({
+        title: title.value,
+        date: date.value,
+        description: description.value,
+        id: uuid,
+      });
+      console.log(this);
+      modal.classList.remove("active");
+      overlay.classList.remove("active");
+    };
+
+    myLibrary.editNote = function (note, notetitle, notedate, notedescription) {
+      let itemID = note.getAttribute("data-id");
+
+      let editedObject = {
+        title: `${notetitle}`,
+        date: `${notedate}`,
+        description: `${notedescription}`,
+      };
+
+      for (let key in myLibrary) {
+        if (Array.isArray(myLibrary[key])) {
+          const index = myLibrary[key].findIndex((item) => (item.id = itemID));
+          if (index !== -1) {
+            myLibrary[key][index] = editedObject;
+            break;
+          }
+        }
+      }
+    };
+
+    myLibrary.deleteNote = function (removeBtn) {
+      let idValue = removeBtn.closest("[data-id]").getAttribute("data-id");
+      for (let key in myLibrary) {
+        if (Array.isArray(myLibrary[key])) {
+          const index = myLibrary[key].findIndex((item) => item.id == idValue);
+          if (index !== -1) {
+            myLibrary[key].splice(index, 1);
+            return;
+          }
+        }
+      }
+    };
+
+    myLibrary.newProject = function (uuid) {
+      myLibrary[uuid] = [];
+    };
+
+    myLibrary.getAllObjects = function () {
+      const objects = [];
+
+      for (let key in this) {
+        if (this.hasOwnProperty(key) && Array.isArray(this[key])) {
+          objects.push(...this[key]);
+        }
+      }
+
+      console.log(objects);
+      return objects;
+    };
+
+    myLibrary.saveToLocalStorage = function () {
+      localStorage.setItem("myLibrary", JSON.stringify(this));
+    };
+
+    Object.assign(myLibrary, savedLibrary);
+  }
+}
